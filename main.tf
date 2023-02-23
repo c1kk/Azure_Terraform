3d99bd51a56abafca02cb512d4517fc58909512d
c1kk <19521694@gm.uit.edu.vn>
2023-02-24 02:53:42 +0700

    Final

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "azurerm" {
+    version = "3.0.1"
+    features {}
+}
+
+resource "azurerm_resource_group" "terra_test" {
+  name = "gr_terra"
+  location = "East US"
+}
+
+resource "azurerm_container_group" "terra_test_2" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.terra_test.location
+  resource_group_name       = azurerm_resource_group.terra_test.name
+
+  ip_address_type     = "Public"
+  dns_name_label      = "caokhoanguyen1"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "caokhoanguyen/weatherapi"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
